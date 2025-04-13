eb431d9a8e675a3607906668181ae9c4c34c9c03
Martin Philipp <martin@teralis.io>
2025-04-13 22:50:40 +1000

    Minor customisation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/teralis/qr-docs',
         },
         blog: {
           showReadingTime: true,
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/teralis/qr-docs',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -68,12 +68,13 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'QR Tub | Help Centre',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'QR Tub Logo',
         src: 'img/logo.svg',
+        href: 'docs/intro',
       },
-      items: [
+      /*items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
@@ -86,11 +87,11 @@ const config: Config = {
           label: 'GitHub',
           position: 'right',
         },
-      ],
+      ],*/
     },
     footer: {
       style: 'dark',
-      links: [
+      /*links: [
         {
           title: 'Docs',
           items: [
@@ -130,8 +131,8 @@ const config: Config = {
             },
           ],
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      ],*/
+      copyright: `Copyright © ${new Date().getFullYear()} QR Tub by Teralis`,
     },
     prism: {
       theme: prismThemes.github,
