8be2debbf36d423a9e1f27989874b4faa80479bd
Martin Philipp <martin@teralis.io>
2025-05-19 16:18:26 +1000

    changed url from help.qrtub.com to www.qrtub.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://help.qrtub.com',
+  url: 'https://www.qrtub.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
